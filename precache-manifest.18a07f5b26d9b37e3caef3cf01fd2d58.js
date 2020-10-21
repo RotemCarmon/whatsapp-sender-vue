92700e10fd73f4d8f1ea1137860a5e9cd307d5fe
Rotem Carmon <tetch6@gmail.com>
2020-10-21 21:51:17 +0300

    gh-pages

---
+++ precache-manifest.18a07f5b26d9b37e3caef3cf01fd2d58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb5ce7cfded384891eb",
+    "url": "/whatsapp-sender-vue/css/app.d3ca7464.css"
+  },
+  {
+    "revision": "fc1be8a1592680e4b85f135de2644da2",
+    "url": "/whatsapp-sender-vue/index.html"
+  },
+  {
+    "revision": "6eb5ce7cfded384891eb",
+    "url": "/whatsapp-sender-vue/js/app.7dfe5f34.js"
+  },
+  {
+    "revision": "101fc160f8d18a366d74",
+    "url": "/whatsapp-sender-vue/js/chunk-vendors.a7be4480.js"
+  },
+  {
+    "revision": "0d5d4f96962fbd87833b72d287207835",
+    "url": "/whatsapp-sender-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/whatsapp-sender-vue/robots.txt"
+  }
+]);
